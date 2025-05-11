fff5910bc8ed5924e0b3c4e5f4a4e9d2f2ebe0ea
Joel Drake <joeldrak3@outlook.com>
2025-05-11 21:15:09 +0200

    word tweaks

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -2,19 +2,22 @@
 	<div class="about-inner">
 		<p>I’m a <strong>Senior Web Developer</strong> based in Stockholm, Sweden.</p>
 		<p>
-			I have been working in tech since 2008. Did some heavy-duty PHP + jQuery back in the day and
-			eventually got on the React bandwagon.
+			I have been working in tech since 2008. Did some PHP + jQuery back in the day and eventually
+			got on the React bandwagon.
 		</p>
 		<p>
 			Since 2019, I’ve been part of a team building a web app for a major retail company in Sweden.
-			We use Vue + TypeScript + GraphQL, and it's a joy to work with. I take pride in calling myself
-			a <strong>front-end expert.</strong>
+			We use Vue + TypeScript + GraphQL, and it's a joy to work with. I consider myself a
+			<strong>front-end expert.</strong>
 		</p>
 		<p>
-			Big fan of open source in web development in general, and projects like Svelte, Vue, Deno and
-			Preact in particular.
+			Big fan of open source in general, and projects like Svelte, Vue, Deno and Preact in
+			particular.
 		</p>
-		<p>I like to be where there are good people and modern tech.</p>
+		<p>
+			Currently finding excitement in exploring ways to use AI as a tool to help me and my team ship
+			new stuff faster and smarter.
+		</p>
 	</div>
 </section>
 
